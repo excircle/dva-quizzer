a0a0ae0adf0659c8239f306f9350661358a08019
Alexander Kalaj <adkalaj@gmail.com>
2023-07-12 14:20:51 -0700

    react-hook-forms functional

--- pages/components/modules/Questions.js
+++ pages/components/modules/Questions.js
@@ -10,95 +10,124 @@ import { useForm, Controller } from "react-hook-form";
 
 const domains = [
   {
+    id: 1,
     value: "Domain 1: AWS Development",
     label: "Domain 1: AWS Development",
   },
   {
+    id: 2,
     value: "Domain 2: AWS Security",
     label: "Domain 2: AWS Security",
   },
   {
+    id: 3,
     value: "Domain 3: AWS Deployment",
     label: "Domain 3: AWS Deployment",
   },
   {
+    id: 4,
     value: "Domain 4: AWS Troubleshooting",
     label: "Domain 4: AWS Troubleshooting",
   },
 ];
 
 export default function Questions() {
+  const { register, handleSubmit, control } = useForm();
+
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
-        Question Submit Form
+        Questions Submit Form
       </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <TextField
-            id="outlined-select-currency-native"
-            select
-            label="Domains"
-            defaultValue="EUR"
-            SelectProps={{
-              native: true,
-            }}
-            helperText="Please select your domain"
-          >
-            {domains.map((option) => (
-              <option key={option.value} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </TextField>
+      <form
+        noValidate
+        onSubmit={handleSubmit((data) => alert(JSON.stringify(data)))}
+      >
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <TextField
+              id="domains"
+              required
+              {...register("domain")}
+              select
+              label="Domains"
+              SelectProps={{
+                native: true,
+              }}
+              helperText="Please select your domain"
+            >
+              {domains.map((option) => (
+                <option key={option.id} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              id="question"
+              name="question"
+              label="Question"
+              {...register("question")}
+              fullWidth
+              autoComplete="Please enter your question..."
+              variant="standard"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              id="answer"
+              name="answer"
+              label="Answer"
+              {...register("answer")}
+              fullWidth
+              autoComplete="Please provide an answer..."
+              variant="standard"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              id="description"
+              name="description"
+              label="Description"
+              {...register("description")}
+              multiline
+              fullWidth
+              autoComplete="Please provide a description..."
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormControlLabel
+              control={
+                <Controller
+                  render={({ field }) => (
+                    <Checkbox
+                      color="secondary"
+                      name="miscellaneous"
+                      value="yes"
+                      {...field}
+                    />
+                  )}
+                  control={control}
+                  name="miscellaneous"
+                />
+              }
+              label="Miscellaneous Question"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              type="Submit"
+              variant="contained"
+              color="inherit"
+              endIcon={<SendIcon />}
+            >
+              Submit
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="question"
-            name="question"
-            label="Question"
-            fullWidth
-            autoComplete="Please enter your question..."
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="answer"
-            name="answer"
-            label="Answer"
-            fullWidth
-            autoComplete="Please provide an answer..."
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="description"
-            name="description"
-            label="Description"
-            multiline
-            fullWidth
-            rows={5}
-            maxRows={10}
-            autoComplete="Please provide a description..."
-            variant="outlined"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={
-              <Checkbox color="secondary" name="saveAddress" value="yes" />
-            }
-            label="Miscellaneous Question"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Button variant="contained" color="inherit" endIcon={<SendIcon />}>
-            Submit
-          </Button>
-        </Grid>
-      </Grid>
+      </form>
     </React.Fragment>
   );
 }
